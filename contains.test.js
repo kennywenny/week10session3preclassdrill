d45008d97e5bb5f4b1f48b7c9acb84d8f5f02ae7
Kenny Hung (he) <khung@instructors.2u.com>
2022-11-28 18:51:06 +0000

    Final set of tests with strings

--- contains.test.js
+++ contains.test.js
@@ -4,28 +4,45 @@ describe('contains function', () => {
   it('should pick out an element contained in an array of numbers', () => {
     const source = [1, 2, 3]
     const expected = true
-    
+
     const actual = contains(source, 1)
 
     expect(expected).toBe(actual)
   })
-  
+
   it('should detect non-member element of an array of numbers', () => {
     const source = [1, 2, 3]
     const expected = false
-    
+
     const actual = contains(source, 4)
 
     expect(expected).toBe(actual)
+  })
+
+  it('should pick out an element in an array of strings', () => {
+    const source = ['code', 'dev', 'nerd']
+    const element = 'nerd'
+    const expected = true
+    
+    const actual = contains(source, element)
+
+    expect(actual).toBe(expected)
+  })
+
+  it('should detect an element not in an array of strings', () => {
+    const source = ['code', 'dev', 'nerd']
+    const element = 'genius'
+    const expected = false
+    
+    const actual = contains(source, element)
+
+    expect(actual).toBe(expected)
   })
 })
 /*
 Test cases:
 
 Ex:
-Input: [1,2,3]  4
-Output: false
-
 Input: ['code', 'dev', 'nerd']  'nerd'
 Output: true
 
