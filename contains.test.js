7e3c96688d7b660d1dd9238be6f152a7a6350bfb
Kenny Hung (he) <khung@instructors.2u.com>
2022-11-28 18:40:52 +0000

    Copy test cases into code

---
+++ contains.test.js
@@ -0,0 +1,16 @@
+/*
+Test cases:
+
+Ex:
+Input: [1,2,3]  1
+Output: true
+
+Input: [1,2,3]  4
+Output: false
+
+Input: ['code', 'dev', 'nerd']  'nerd'
+Output: true
+
+Input: ['code', 'dev', 'nerd']  'genius'
+Output: false
+*/
